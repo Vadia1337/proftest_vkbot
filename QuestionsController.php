bf9612ac68ef3e33617d430f8677e078bd553859
vadiucha <vad.orov@gmail.com>
2022-04-13 16:26:17 +0300

    Added new functions for saving test results to a file, developed a Checkbox-type selection function

--- QuestionsController.php
+++ QuestionsController.php
@@ -2,9 +2,9 @@
 
 if ($data->type == 'message_new') {
 
- //   if ($message == 'Начать' OR $message == 'начать') {
+    if ($message == 'Начать' OR $message == 'начать' OR $message == 'Старт') {
         mainmenu($vk, $peer_id);
- //    }
+     }
 }
 
 if (isset($data->object->payload)) {  //получаем payload
@@ -34,6 +34,10 @@ if ($payload == 'btn_5'){
     nextQ($vk, $peer_id);
 }
 
+if ($payload == 'btn_6'){
+    Checkbox($vk, $peer_id);
+}
+
 
 
 
@@ -42,27 +46,98 @@ function mainmenu($vk, $peer_id){
     $btn_1 = $vk->buttonText('Пройти тест', 'green', ['command' => 'btn_1']);
     $btn_2 = $vk->buttonText('Результаты теста', 'blue', ['command' => 'btn_2']);
     $btn_3 = $vk->buttonText('Пройти заново', 'white', ['command' => 'btn_3']);
-    $btn_4 = $vk->buttonText('Выйти', 'red', ['command' => 'btn_4']);
+    $btn_4 = $vk->buttonText('Выйти', 'red', ['command' => 'btn_9']);
 
-    $vk->sendButton($peer_id, "Приветствую, я бот Политеша, предлагаю тебе пройти быстрый тест, после я смогу подсказать тебе на какую специальность лучше подать документы :-)", [[$btn_1], [$btn_2], [$btn_3, $btn_4]]);
+    $vk->sendButton($peer_id, "Приветствую, предлагаю тебе пройти быстрый тест, после я смогу подсказать тебе на какую специальность лучше подать документы :-)", [[$btn_1], [$btn_2], [$btn_3, $btn_4]]);
 }
 
 // вопрос №1 
 function nextQ($vk, $peer_id){
 
-    $btn_1 = $vk->buttonText('Да', 'green', ['command' => 'KIS_1']);
-    $btn_2 = $vk->buttonText('Нет', 'blue', ['command' => 'IB_1']);
+    $btn_1 = $vk->buttonText('Да', 'green', ['command' => 'Test_KIS_1']);
+    $btn_2 = $vk->buttonText('Нет', 'blue', ['command' => 'Test_IB_1']);
     $btn_3 = $vk->buttonText('Не оч', 'white', ['command' => 'btn_3']);
-    $btn_4 = $vk->buttonText('Выйти', 'red', ['command' => 'btn_4']);
+    $btn_4 = $vk->buttonText('Выйти', 'red', ['command' => 'btn_9']);
 
     $vk->sendButton($peer_id, "Тебе нравиться прога?", [[$btn_1], [$btn_2], [$btn_3, $btn_4]]);
 }
 
-if ($payload == 'KIS_1'){
-    //записать в бд
+function Checkbox($vk, $peer_id){
+
+    $btn_1 = $vk->buttonText('1', 'green', ['command' => 'Check_btn1_KIS_1']);
+    $btn_2 = $vk->buttonText('2', 'green', ['command' => 'Check_btn2_IB_1']);
+    $btn_3 = $vk->buttonText('3', 'green', ['command' => 'Check_btn3_KIS_1']);
+    $btn_4 = $vk->buttonText('4', 'green', ['command' => 'Check_btn4_KIS_1']);
+    $btn_5 = $vk->buttonText('Отправить', 'blue', ['command' => 'CheckBoxSend']);
+
+    $vk->sendButton($peer_id, "Выбери варианты ответов", [[$btn_1, $btn_2], [$btn_3, $btn_4], [$btn_5]]);
 }
-if ($payload == 'IB_1'){
-    //записать в бд
+
+if (str_contains($payload, 'Check')){
+if(str_contains($payload, 'btn1')){
+    if(str_contains($payload, 'Checked_btn1')){
+        $payload = substr_replace($payload, '', strpos($payload, 'Checked_btn1'), 18);
+        $btn_1 = $vk->buttonText('1', 'green', ['command' => $payload.'Check_btn1_KIS_1']);
+    }else{
+    $payload1 = substr_replace($payload, 'Checked', strpos($payload, 'Check_btn1'), 5);
+        $btn_1 = $vk->buttonText('1', 'red', ['command' => $payload1]);
+    }
+}else{
+        $btn_1 = $vk->buttonText('1', 'green', ['command' => $payload.'Check_btn1_KIS_1']);
+}
+
+
+if(str_contains($payload, 'btn2')){
+    if(str_contains($payload, 'Checked_btn2')){
+        $payload = substr_replace($payload, '', strpos($payload, 'Checked_btn2'), 17);
+        $btn_2 = $vk->buttonText('2', 'green', ['command' => $payload.'Check_btn2_IB_1']);
+    }else{
+    $payload2 = substr_replace($payload, 'Checked', strpos($payload, 'Check_btn2'), 5);
+        $btn_2 = $vk->buttonText('2', 'red', ['command' => $payload2]);
+    }
+}else{
+        $btn_2 = $vk->buttonText('2', 'green', ['command' => $payload.'Check_btn2_IB_1']);
+}
+
+
+if(str_contains($payload, 'btn3')){
+    if(str_contains($payload, 'Checked_btn3')){
+        $payload = substr_replace($payload, '', strpos($payload, 'Checked_btn3'), 18);
+        $btn_3 = $vk->buttonText('3', 'green', ['command' => $payload.'Check_btn3_KIS_1']);
+    }else{
+    $payload3 = substr_replace($payload, 'Checked', strpos($payload, 'Check_btn3'), 5);
+        $btn_3 = $vk->buttonText('3', 'red', ['command' => $payload3]);
+    }
+}else{
+        $btn_3 = $vk->buttonText('3', 'green', ['command' => $payload.'Check_btn3_KIS_1']);
+}
+
+
+if(str_contains($payload, 'btn4')){
+    if(str_contains($payload, 'Checked_btn4')){
+        $payload = substr_replace($payload, '', strpos($payload, 'Checked_btn4'), 18);
+        $btn_4 = $vk->buttonText('4', 'green', ['command' => $payload.'Check_btn4_KIS_1']);
+    }else{
+    $payload4 = substr_replace($payload, 'Checked', strpos($payload, 'Check_btn4'), 5);
+        $btn_4 = $vk->buttonText('4', 'red', ['command' => $payload4]);
+    }
+}else{
+        $btn_4 = $vk->buttonText('4', 'green', ['command' => $payload.'Check_btn4_KIS_1']);
+}
+
+        $btn_5 = $vk->buttonText('Отправить', 'blue', ['command' => 'CheckBoxSend_']);
+
+    $vk->sendButton($peer_id, "0".$payload, [[$btn_1, $btn_2], [$btn_3, $btn_4], [$btn_5]]);
+//&#4448;
+}
+
+
+
+if (str_contains($payload, 'Test')){
+    $fileredactor = fopen('tests/'.$peer_id.".txt", "c");
+    fseek($fileredactor, 0, SEEK_END); 
+    fwrite($fileredactor, substr($payload, 5)); //обрезка Test_
+    fclose($fileredactor);
 }
 
 ?>
